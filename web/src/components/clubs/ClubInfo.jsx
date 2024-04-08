91cbbb61a21ddb5a99d91d289ae701c87ad70e53
Bhav Beri <bhavberi@gmail.com>
2024-04-08 11:24:28 +0530

    Render the description with the same spacing and new lines as given

--- web/src/components/clubs/ClubInfo.jsx
+++ web/src/components/clubs/ClubInfo.jsx
@@ -47,7 +47,7 @@ export default function ClubInfo({
         </Box>
       </Box>
       <Box pt={2}>
-        <Typography variant="body1" mx={1} color="text.secondary">
+        <Typography variant="body1" mx={1} color="text.secondary" sx={{ whiteSpace: "pre-wrap" }}>
           {description}
         </Typography>
       </Box>
